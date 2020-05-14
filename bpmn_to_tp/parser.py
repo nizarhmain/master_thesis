4b957c1ec5d6a592da87387024b375b033f3df18
nizarhmain <hmain.nizar@gmail.com>
2020-05-14 18:29:28 +0200

    working on bpmn to python parsing logic

--- bpmn_to_tp/parser.py
+++ bpmn_to_tp/parser.py
@@ -19,10 +19,24 @@ def trimb(string):
 
     return elem
 
+# trim until lower dash is found
+
+
+def triml(string):
+    trimmed = string[:string.find('_')]
+    # print(trimmed[1:])
+    elem = trimmed[0:]
+    return elem
+
 
 messages = []
 participants = []
 messageFlows = []
+choreographyTasks = []
+sequenceFlows = []
+exclusiveGateways = []
+parallelGateways = []
+eventBasedGateways = []
 
 for child in root:
     # print(child.tag)
@@ -52,6 +66,90 @@ for child in root:
             if subchild_type == 'messageFlow':
                 messageFlows.append(subchild.attrib)
 
+            if subchild_type == 'choreographyTask':
+                choreographyTasks.append(subchild)
+
+            if subchild_type == 'sequenceFlow':
+                # print(subchild.attrib)
+                sequenceFlows.append(subchild)
+
+            if subchild_type == 'exclusiveGateway':
+                # print(subchild.attrib)
+                exclusiveGateways.append(subchild)
+
+            if subchild_type == 'parallelGateway':
+                print(subchild.attrib)
+                parallelGateways.append(subchild)
+
+            if subchild_type == 'eventBasedGateway':
+                print(subchild.attrib)
+                eventBasedGateways.append(subchild)
+
+
+
+def findOutgoings(id):
+    # figure out the type
+
+    elementType = triml(id)
+    print(elementType)
+
+    if elementType == 'SequenceFlow':
+        for seq in sequenceFlows:
+            if seq.attrib['id'] == id:
+                print(seq.attrib)
+                findOutgoings(seq.attrib['sourceRef'])
+    
+    if elementType == 'ExclusiveGateway':
+        for x in exclusiveGateways:
+            if x.attrib['id'] == id:
+                for y in x:
+                    if trimb(y.tag) == 'incoming':
+                        print(y.text)
+                        findOutgoings(y.text)
+
+    if elementType == 'ChoreographyTask':
+        for x in choreographyTasks:
+            if x.attrib['id'] == id:
+                print(x.attrib['name'])
+
+    
+    if elementType == 'EventBasedGateway':
+        for x in eventBasedGateways:
+            if x.attrib['id'] == id:
+                for y in x:
+                    if trimb(y.tag) == 'incoming':
+                        print(y.text)
+                        findOutgoings(y.text)
+
+    if elementType == 'ParallelGateway':
+        for x in parallelGateways:
+            if x.attrib['id'] == id:
+                for y in x:
+                    if trimb(y.tag) == 'incoming':
+                        print(y.text)
+                        findOutgoings(y.text)
+
+
+
+
+def preconditions_choreotask(id):
+
+    for task in choreographyTasks:
+        if task.attrib['id'] == id:
+            for child in task:
+
+                if trimb(child.tag) == 'incoming':
+                    print(child.text)
+                    # this is the id of the sequence flow
+                    findOutgoings(child.text)
+                    # find all the outgoing ones that are equal to this one
+
+                if trimb(child.tag) == 'outgoing':
+                    print('nevermind this')
+
+            # figure out the incoming and the outcoming of these task
+            # since this can only come from one way
+
 
 # print(participants)
 # print(messages)
@@ -79,8 +177,27 @@ for msgflow in messageFlows:
 
     message = findMsgRef(messages, msgflow['messageRef'])
 
-    print('%s ----> %s ----> %s.'%(sourceguy['name'],message['name'], targetguy['name']))
+    print('%s ----> %s ----> %s.' %
+          (sourceguy['name'], message['name'], targetguy['name']))
 
 
 # messages.append(child.attrib)
 
+
+# preconditions_choreotask('ChoreographyTask_1ivug1p')
+
+# preconditions_choreotask('ChoreographyTask_137ic1s')
+
+# preconditions_choreotask('ChoreographyTask_1j5c18h')
+
+# damage refund
+# preconditions_choreotask('ChoreographyTask_00tsj0o')
+
+# give feedback
+# preconditions_choreotask('ChoreographyTask_1buzavy')
+
+# give credits
+# preconditions_choreotask('ChoreographyTask_0tydxhk')
+
+# initial requ
+preconditions_choreotask('ChoreographyTask_1tcylad')
--- sol_from_bpmn/solfiles/diagram.sol
+++ sol_from_bpmn/solfiles/diagram.sol
@@ -1,148 +1,137 @@
 pragma solidity ^0.4.14;
-        
-    
-        
-    contract Process_0_Contract {
-        uint tokens = 2;
-        address owner = 0;
-        address parent = 0;
-        uint subprocesses = 0;
-        uint [] requestedID;
-                    event Element_Execution_Completed(uint elementId);
-            Process_0_WorkList workList = new Process_0_WorkList();
-            uint active_firstask = 0;
-                        
-        function Process_0_Contract() {
-            owner = msg.sender;
-                for (uint i = 0; i < 0; i++)
-                requestedID.push(0);
-            step(tokens);
+
+
+contract Process_0_Contract {
+    uint256 tokens = 2;
+    address owner = 0;
+    address parent = 0;
+    uint256 subprocesses = 0;
+    uint256[] requestedID;
+    event Element_Execution_Completed(uint256 elementId);
+    Process_0_WorkList workList = new Process_0_WorkList();
+    uint256 active_a = 0;
+
+    function Process_0_Contract() {
+        owner = msg.sender;
+        for (uint256 i = 0; i < 0; i++) requestedID.push(0);
+        step(tokens);
+    }
+
+    function setParent(address newParent) {
+        if (owner == msg.sender) parent = newParent;
+    }
+
+    function handleGlobalDefaultEnd() {
+        // ................ Nothing to do ...........
+    }
+
+    function handleGlobalErrorEnd(bytes32 eventName) {
+        if (parent != 0)
+            Process_0_Contract(parent).handleGlobalErrorEnd(eventName);
+        else tokens &= uint256(~kill_Process_0());
+    }
+
+    function handleGlobalEscalationEnd(bytes32 eventName) {
+        if (parent != 0)
+            Process_0_Contract(parent).handleGlobalEscalationEnd(eventName);
+    }
+
+    function kill_Process_0() returns (uint256) {
+        uint256 tokensToKill = 0;
+        tokensToKill |= uint256(6);
+        active_a = 0;
+        tokens &= uint256(~tokensToKill);
+        return 0;
+    }
+
+    function broadcastSignal_Process_0() {
+        // Nothing to do ...
+    }
+
+    function a_start(uint256 localTokens) internal returns (uint256) {
+        uint256 reqId = workList.DefaultTask_start(this.a_callback);
+        active_a |= uint256(1) << reqId;
+        return localTokens & uint256(~2);
+    }
+
+    function a_callback(uint256 reqId) returns (bool) {
+        if (active_a == 0) return false;
+        uint256 index = uint256(1) << reqId;
+        if (active_a & index == index) {
+            active_a &= ~index;
+
+            step(tokens | 4);
+            Element_Execution_Completed(1);
+            return true;
         }
-    
-        function setParent(address newParent) {
-            if (owner == msg.sender)
-                parent = newParent;
+        return false;
+    }
+
+    function EndEvent_1rfz6w8(uint256 localTokens) internal returns (uint256) {
+        tokens = localTokens & uint256(~4);
+        if (tokens & 6 != 0) {
+            return tokens;
         }
-    
-            function handleGlobalDefaultEnd() {
-                // ................ Nothing to do ...........
+        if (parent != 0) Process_0_Contract(parent).handleGlobalDefaultEnd();
+        Element_Execution_Completed(2);
+        return tokens;
+    }
+
+    function step(uint256 localTokens) internal {
+        bool done = false;
+        while (!done) {
+            if (localTokens & 2 == 2) {
+                localTokens = a_start(localTokens);
+                continue;
             }
-    
-            function handleGlobalErrorEnd(bytes32 eventName) {
-                if (parent != 0)
-                Process_0_Contract(parent).handleGlobalErrorEnd(eventName);
-                else
-                tokens &= uint(~kill_Process_0());
-             }
-    
-            function handleGlobalEscalationEnd(bytes32 eventName) {
-                if (parent != 0)
-                Process_0_Contract(parent).handleGlobalEscalationEnd(eventName);
-             }
-    
-                                function kill_Process_0() returns (uint) {
-            uint tokensToKill = 0;
-                tokensToKill |= uint(6);
-                active_firstask = 0;
-                    tokens &= uint(~tokensToKill);
-            return 0;   
+            if (localTokens & 4 == 4) {
+                localTokens = EndEvent_1rfz6w8(localTokens);
+                continue;
             }
-    
-             function broadcastSignal_Process_0() {
-            // Nothing to do ...
+            done = true;
         }
-    
-                        
-        function firstask_start (uint localTokens) internal returns (uint) {
-            uint reqId = workList.DefaultTask_start (this.firstask_callback);
-            active_firstask |= uint(1) << reqId;
-                return localTokens & uint(~2);
-            }
-    
-        function firstask_callback (uint reqId) returns (bool) {
-            if (active_firstask == 0) 
-                return false;
-            uint index = uint(1) << reqId;
-            if(active_firstask & index == index) {
-                active_firstask &= ~index;
-                    
-                step (tokens | 4);
-                    Element_Execution_Completed(1);
-                return true;
-            }
-            return false ;
-        }
-    
-            function Event_1y9xp5v(uint localTokens) internal returns (uint) {
-            tokens = localTokens & uint(~4);
-                if (tokens & 6 != 0) {
-                    return tokens;
-            }
-                if (parent != 0)
-                Process_0_Contract(parent).handleGlobalDefaultEnd();
-                Element_Execution_Completed(2);
-                return tokens;
-        }
-    
-                function step(uint localTokens) internal { 
-            bool done = false;
-            while (!done) {
-                    if (localTokens & 2 == 2) {
-                    localTokens = firstask_start(localTokens);
-                    continue;
-                }
-                    if (localTokens & 4 == 4) {
-                    localTokens = Event_1y9xp5v(localTokens); 
-                    continue;
-                }
-                        done = true;
-            }
-            tokens = localTokens;
-        }
-     
-        function getRunningFlowNodes() returns (uint) {
-            uint flowNodes = 0;
-            uint localTokens = tokens;
-                return flowNodes;
-        }
-    
-            function getStartedFlowNodes() returns (uint) {
-            uint flowNodes = 0;
-            uint localTokens = tokens;
-                if(active_firstask != 0)
-                flowNodes |= 1;
-                
-                    return flowNodes;
-        }
-    
-        function getWorkListAddress() returns (address) {
-            return workList;
-        }
-    
-        function getTaskRequestIndex(uint taskId) returns (uint) { 
-                if (taskId == 1)
-                return active_firstask;
-            }
-    
-            }
-    
-    
+        tokens = localTokens;
+    }
+
+    function getRunningFlowNodes() returns (uint256) {
+        uint256 flowNodes = 0;
+        uint256 localTokens = tokens;
+        return flowNodes;
+    }
+
+    function getStartedFlowNodes() returns (uint256) {
+        uint256 flowNodes = 0;
+        uint256 localTokens = tokens;
+        if (active_a != 0) flowNodes |= 1;
+
+        return flowNodes;
+    }
+
+    function getWorkListAddress() returns (address) {
+        return workList;
+    }
+
+    function getTaskRequestIndex(uint256 taskId) returns (uint256) {
+        if (taskId == 1) return active_a;
+    }
+}
+
 
 contract Process_0_WorkList {
-            struct DefaultTask_Request {
-            function (uint) external returns (bool) callback;    
-        }
-        DefaultTask_Request [] DefaultTask_requests;
-    
-        function DefaultTask_start (function (uint) external returns (bool) callback) returns (uint) {
-            uint index = DefaultTask_requests.length;
-            DefaultTask_requests.push(DefaultTask_Request(callback));
-            return index;
-        }
-        
-        function DefaultTask_callback (uint reqId) {
-            DefaultTask_requests[reqId].callback(reqId);
-        }
-    
-        }
-    
+    struct DefaultTask_Request {
+        function(uint256) returns (bool) external callback;
+    }
+    DefaultTask_Request[] DefaultTask_requests;
+
+    function DefaultTask_start(
+        function(uint256) returns (bool) external callback
+    ) returns (uint256) {
+        uint256 index = DefaultTask_requests.length;
+        DefaultTask_requests.push(DefaultTask_Request(callback));
+        return index;
+    }
+
+    function DefaultTask_callback(uint256 reqId) {
+        DefaultTask_requests[reqId].callback(reqId);
+    }
+}
