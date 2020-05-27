02a52cd5427e235f6df49e7aca400509777da4b3
nizarhmain <hmain.nizar@gmail.com>
2020-05-27 21:48:44 +0200

    script for testing private reads on simple storage sol

--- quorum/scripts/node_info.py
+++ quorum/scripts/node_info.py
@@ -5,6 +5,17 @@
 import requests
 import json
 
+from web3 import Web3, HTTPProvider, IPCProvider, WebsocketProvider
+
+
+NODE_PORT = 22002 
+
+PRIVATE_ABI = '[{"constant":true,"inputs":[],"name":"storedData","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"x","type":"uint256"}],"name":"set","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"get","outputs":[{"name":"retVal","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"initVal","type":"uint256"}],"payable":false,"type":"constructor"}]'
+
+FIRST_TX = '0x58d58bf5b1bfc3ce5270953d4ff3c1067177390d86ac8ba974a1f6f286859473'
+
+w3 = Web3(HTTPProvider(f'http://localhost:{NODE_PORT}'))
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -57,22 +68,43 @@ def rpc_method(method, params, port, cleanhex=False, pretty_json=False):
 		print(f'{bcolors.FAIL}{response}{bcolors.ENDC}')
 
 
-rpc_method('web3_clientVersion', [], 22000)
-rpc_method('eth_syncing', [], 22000)
-rpc_method('net_version', [], 22000)
-rpc_method('net_listening', [], 22000)
-rpc_method('net_peerCount', [], 22000, True)
-rpc_method('eth_protocolVersion', [], 22000, True)
-rpc_method('eth_coinbase', [], 22000)
-rpc_method('eth_mining', [], 22000)
-rpc_method('eth_hashrate', [], 22000)
-rpc_method('eth_gasPrice', [], 22000)
-first_acc = rpc_method('eth_accounts', [], 22000)
-rpc_method('eth_blockNumber', [], 22000)
-rpc_method('eth_getBalance', [first_acc[0], 'latest'], 22000, True)
-rpc_method('eth_getTransactionCount', [first_acc[0], 'latest'], 22000, True)
-rpc_method('eth_getTransactionByHash', ['0x58d58bf5b1bfc3ce5270953d4ff3c1067177390d86ac8ba974a1f6f286859473'], 22000, False, True)
-rpc_method('eth_getTransactionReceipt', ['0x58d58bf5b1bfc3ce5270953d4ff3c1067177390d86ac8ba974a1f6f286859473'], 22000, False, True)
+rpc_method('web3_clientVersion', [], NODE_PORT)
+rpc_method('eth_syncing', [], NODE_PORT)
+rpc_method('net_version', [], NODE_PORT)
+rpc_method('net_listening', [], NODE_PORT)
+rpc_method('net_peerCount', [], NODE_PORT, True)
+rpc_method('eth_protocolVersion', [], NODE_PORT, True)
+rpc_method('eth_coinbase', [], NODE_PORT)
+rpc_method('eth_mining', [], NODE_PORT)
+rpc_method('eth_hashrate', [], NODE_PORT)
+rpc_method('eth_gasPrice', [], NODE_PORT)
+
+first_acc = rpc_method('eth_accounts', [], NODE_PORT)
+rpc_method('eth_blockNumber', [], NODE_PORT)
+rpc_method('eth_getBalance', [first_acc[0], 'latest'], NODE_PORT, True)
+rpc_method('eth_getTransactionCount', [first_acc[0], 'latest'], NODE_PORT, True)
+
+rpc_method('eth_getTransactionByHash', [FIRST_TX], NODE_PORT, False, True)
+contract_addr = rpc_method('eth_getTransactionReceipt', [FIRST_TX], NODE_PORT, False, True)['contractAddress']
+
+# needed for the thing, otherwise it complains
+checksum_contract_addr = Web3.toChecksumAddress(contract_addr)
+
+print(f'{bcolors.WARNING}contract address is : {contract_addr} {bcolors.ENDC}')
+
+# Let's try to call the Get on the contract
+
+# print(Web3.isChecksumAddress(checksum_contract_addr))
+
+private_contract = w3.eth.contract(address=checksum_contract_addr, abi=PRIVATE_ABI)
+
+try:
+	final_value = private_contract.functions.get().call()
+	print(f'{bcolors.WARNING}private value is : {final_value} {bcolors.ENDC}')
+except Exception as e:
+	print(f'{bcolors.FAIL} {e} {bcolors.ENDC}')
+
+
 
 
 # private contract address
